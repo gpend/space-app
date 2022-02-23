7b831be3c0673fd3c1a38236b895f97d29d97c2e
Greg Pendleton <gpendleton@gmail.com>
2022-02-22 20:35:43 -0600

    started nav

---
+++ src/components/nav.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Nav (){
+    return(
+        <nav>
+            <ul>
+                <li>
+                    00 HOME
+                </li>
+                <li>
+                    01 DESTINATION
+                </li>
+            </ul>
+        </nav>
+    )
+}
