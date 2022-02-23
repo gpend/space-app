8efa209c582c9bd840398fa92d2b87d7d9ebf674
Greg Pendleton <gpendleton@gmail.com>
2022-02-23 10:22:05 -0600

    updated nav, started home and setup routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 import Home from "./components/Home"
+import Nav from "./components/nav"
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Nav />
+      <Routes>
+        <Route path='/' element={
+          <Home />
+        } />
+        <Route path='/destination' element={
+          <Home />
+        } />
+        <Route path='/crew' element={
+          <Home />
+        } />
+        <Route path='/technology' element={
+          <Home />
+        } />
+      </Routes>
+      
     </div>
   );
 }
