e58fa129b9731fbdb04a6edce69ebcd70b34e3aa
greg <gpendleton@gmail.com>
2022-06-27 14:15:34 -0500

    add gh-pages deployment, fix routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import './App.css';
-import Home from "./components/Home"
+import Home from './components/Home';
 import Destination from './components/Destination';
-import Crew from './components/Crew'
+import Crew from './components/Crew';
 import Technology from './components/Technology';
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
       <Routes>
-        <Route path='/' element={ <Home /> } />
-          <Route path='destination/*' element={ <Destination /> } />
-          <Route path='crew/*' element={ <Crew /> } />
-          <Route path='technology/*' element={ <Technology /> } />
+        <Route path='/space-app' element={<Home />} />
+        <Route path='destination/*' element={<Destination />} />
+        <Route path='crew/*' element={<Crew />} />
+        <Route path='technology/*' element={<Technology />} />
       </Routes>
-      
     </div>
   );
 }
--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,52 +1,57 @@
-import { Route, Routes, useParams, useLocation } from "react-router"
-import { Link } from "react-router-dom"
-import Header from "./Header"
-import data from "../assets/shared/data.json"
+import { Route, Routes, useParams, useLocation } from 'react-router';
+import { Link } from 'react-router-dom';
+import Header from './Header';
+import data from '../assets/shared/data.json';
 
-function Destination(props){
-    const location = useLocation()
-    console.log(location.pathname.split("/")[2] ? "true": "false")
-    function FormatBody(){
-        const props = useParams()
-        const bodyData = data.destinations.filter((destination) => destination.name === props.bodyID)
-        const planetaryBody = bodyData[0]
-        return(
-            <div className="planetaryBody subSection" key={planetaryBody.name}>
-                <img src={planetaryBody.images.webp} alt="" />
-                <div className="sub-nav">
-                    <ul>
-                        {data.destinations.map(destination => (
-                            <li key={destination.name}>
-                                <Link to={`/destination/${destination.name}`}>{destination.name}</Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <h1>{planetaryBody.name}</h1>
-                <p>{planetaryBody.description}</p>
-                <hr />
-                <p>AVG. DISTANCE</p>
-                <p>{planetaryBody.distance}</p>
-                <p>EST. TRAVEL TIME</p>
-                <p>{planetaryBody.travel}</p>
-            </div>
-        )
-    }
-
-    return(
-        <div className="destinations sub-app">
-            < Header />
-            <div className="sub-section">
-                <p>
-                    <span className="destination--header-number">01</span> PICK YOUR DESTINATION
-                </p>
-                
-                <Routes>
-                    <Route path=":bodyID" element={<FormatBody />}/>
-                </Routes>
-            </div>
+function Destination(props) {
+  const location = useLocation();
+  console.log(location.pathname.split('/')); //[2] ? "true": "false"
+  function FormatBody() {
+    const props = useParams();
+    const bodyData = data.destinations.filter(
+      (destination) => destination.name === props.bodyID
+    );
+    const planetaryBody = bodyData[0];
+    return (
+      <div className='planetaryBody subSection' key={planetaryBody.name}>
+        <img src={`/space-app/${planetaryBody.images.webp}`} alt='' />
+        <div className='sub-nav'>
+          <ul>
+            {data.destinations.map((destination) => (
+              <li key={destination.name}>
+                <Link to={`/destination/${destination.name}`}>
+                  {destination.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
-    )
+        <h1>{planetaryBody.name}</h1>
+        <p>{planetaryBody.description}</p>
+        <hr />
+        <p>AVG. DISTANCE</p>
+        <p>{planetaryBody.distance}</p>
+        <p>EST. TRAVEL TIME</p>
+        <p>{planetaryBody.travel}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className='destinations sub-app'>
+      <Header />
+      <div className='sub-section'>
+        <p>
+          <span className='destination--header-number'>01</span> PICK YOUR
+          DESTINATION
+        </p>
+
+        <Routes>
+          <Route path=':bodyID' element={<FormatBody />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
-export default Destination
+export default Destination;
