e045fc8e62ab1f83cfe1901926673dc43548cba9
greg <gpendleton@gmail.com>
2022-07-11 20:02:56 -0500

    update class names

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -3,59 +3,57 @@ import { Link } from 'react-router-dom';
 import Header from './Header';
 import data from '../assets/shared/data.json';
 
-function Destination(props) {
-  function FormatBody() {
-    const props = useParams();
-    const bodyData = data.destinations.filter(
-      (destination) => destination.name === props.bodyID
-    );
-    const planetaryBody = bodyData[0];
-    return (
-      <div className='destination--subSection' key={planetaryBody.name}>
-        <div className='image__container'>
-          <img src={`/space-app/${planetaryBody.images.webp}`} alt='' />
-        </div>
-        <div className='sub-nav'>
-          <ul>
-            {data.destinations.map((destination) => (
-              <li key={destination.name}>
-                <Link
-                  to={`/destination/${destination.name}`}
-                  className={
-                    destination.name === props.bodyID
-                      ? 'selected'
-                      : 'unselected'
-                  }
-                >
-                  {destination.name.toUpperCase()}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className='destination bodyText'>
-          <h1>{planetaryBody.name}</h1>
-          <p>{planetaryBody.description}</p>
-          <hr />
-          <div className='destination details'>
-            <div className='destination details distance'>
-              <p>AVG. DISTANCE</p>
-              <p className='destination details bigText'>
-                {planetaryBody.distance}
-              </p>
-            </div>
-            <div className='destination details travelTime'>
-              <p>EST. TRAVEL TIME</p>
-              <p className='destination details bigText'>
-                {planetaryBody.travel}
-              </p>
-            </div>
+function FormatBody() {
+  const props = useParams();
+  const bodyData = data.destinations.filter(
+    (destination) => destination.name === props.bodyID
+  );
+  const planetaryBody = bodyData[0];
+  return (
+    <div className='destination--subSection' key={planetaryBody.name}>
+      <div className='destination--subSection image__container'>
+        <img src={`/space-app/${planetaryBody.images.webp}`} alt='' />
+      </div>
+      <div className='destination--subSection sub-nav'>
+        <ul>
+          {data.destinations.map((destination) => (
+            <li key={destination.name}>
+              <Link
+                to={`/destination/${destination.name}`}
+                className={
+                  destination.name === props.bodyID ? 'selected' : 'unselected'
+                }
+              >
+                {destination.name.toUpperCase()}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className='destination bodyText'>
+        <h1>{planetaryBody.name}</h1>
+        <p>{planetaryBody.description}</p>
+        <hr />
+        <div className='destination details'>
+          <div className='destination details distance'>
+            <p>AVG. DISTANCE</p>
+            <p className='destination details bigText'>
+              {planetaryBody.distance}
+            </p>
+          </div>
+          <div className='destination details travelTime'>
+            <p>EST. TRAVEL TIME</p>
+            <p className='destination details bigText'>
+              {planetaryBody.travel}
+            </p>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
+function Destination(props) {
   return (
     <div className='destinations sub-app'>
       <Header />
