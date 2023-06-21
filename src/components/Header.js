231ac03967c3a1ea14adbfddddae30f3b03a8b79
greg <gpendleton@gmail.com>
2023-06-20 20:22:04 -0500

    style tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,54 +9,56 @@ function Header() {
       <div className='header--icon'>
         <img src={icon} alt='site icon' />
       </div>
-      <nav className='headerNav'>
-        <button onClick={HideNav}>X</button>
-        <ul>
-          <li>
-            <NavLink to={"/space-app/"}>
-              <span className='nav-number' aria-hidden='true'>
-                00
-              </span>{" "}
-              HOME
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={"/destination/Moon"}>
-              <span className='nav-number' aria-hidden='true'>
-                01
-              </span>{" "}
-              DESTINATION
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={"/crew/Hurley"}>
-              <span className='nav-number' aria-hidden='true'>
-                02
-              </span>{" "}
-              CREW
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={"/technology/Launch"}>
-              <span className='nav-number' aria-hidden='true'>
-                03
-              </span>{" "}
-              TECHNOLOGY
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      <div className='hamburgerMenu'>
-        <button id='menuButton' onClick={HideNav}>
-          <img src={hamburgerMenu} alt='menu' />
-        </button>
+      <div className='headerNav'>
+        <nav className='headerNavMenu'>
+          <button onClick={HideNav}>X</button>
+          <ul className='flex'>
+            <li>
+              <NavLink to={"/space-app/"}>
+                <span className='nav-number' aria-hidden='true'>
+                  00
+                </span>{" "}
+                HOME
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={"/destination/Moon"}>
+                <span className='nav-number' aria-hidden='true'>
+                  01
+                </span>{" "}
+                DESTINATION
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={"/crew/Hurley"}>
+                <span className='nav-number' aria-hidden='true'>
+                  02
+                </span>{" "}
+                CREW
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={"/technology/Launch"}>
+                <span className='nav-number' aria-hidden='true'>
+                  03
+                </span>{" "}
+                TECHNOLOGY
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className='hamburgerMenu'>
+          <button id='menuButton' onClick={HideNav}>
+            <img src={hamburgerMenu} alt='menu' />
+          </button>
+        </div>
       </div>
     </header>
   );
 }
 
 function HideNav() {
-  var navMenu = document.getElementsByClassName("headerNav")[0];
+  var navMenu = document.getElementsByClassName("headerNavMenu")[0];
   var hamburgerButton = document.getElementsByClassName("hamburgerMenu")[0];
   if (navMenu.style.display === "block") {
     navMenu.style.display = "none";
